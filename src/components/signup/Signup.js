b8087dc7b4166ac9218c0b5facaabd4ad238bcf3
Amirhosein Asghari <amirhoseinasghari2000@gmail.com>
2022-05-03 01:51:04 +0430

    signup page html destructure

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -1,10 +1,48 @@
 import React from 'react';
 import styles from './Signup.module.css';
+import { Link } from 'react-router-dom';
+
+//tools
+import image from '../../tools/images/Signup.png'
 
 const Signup = () => {
     return (
-        <div>
-           
+        <div className={styles.signupContainer}>
+            <div className={styles.imageSection}>
+                <img src={image} alt='image' />
+            </div>
+
+
+            <div className={styles.formSection}>
+                <form>
+                    <div className={styles.header}>
+                        <p>Already have an account?</p>
+                        <Link to='/signin'>Sign in</Link>
+                    </div>
+                    <div className={styles.heading}>
+                        <h3>Welcome to Focus!</h3>
+                        <p>Register your account</p>
+                    </div>
+                    <div className={styles.formField}>
+                        <label>Name</label>
+                        <input name='name' type='text' placeholder='enter ur username...' />
+                    </div>
+                    <div className={styles.formField}>
+                        <label>Email</label>
+                        <input name='email' type='email' placeholder='example@gmail.com' />
+                    </div>
+                    <div className={styles.formField}>
+                        <label>Password</label>
+                        <input name='password' type='password' placeholder='8+ characters' />
+                    </div>
+                    <div className={styles.formField}>
+                        <label>Confirm password</label>
+                        <input name='confirmPassword' type='password' placeholder='8+ characters' />
+                    </div>
+                    <button>Sign up</button>
+                </form>
+            </div>
+
         </div>
     );
 };
