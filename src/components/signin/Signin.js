7551ff9f3c8b4213f4e08c8ab051788c5a8513d2
Amirhosein Asghari <amirhoseinasghari2000@gmail.com>
2022-05-03 01:42:03 +0430

    project reinitialized with react-router-dom

---
+++ src/components/signin/Signin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Signin.module.css';
+
+const Signin = () => {
+    return (
+        <div>
+            sign in
+        </div>
+    );
+};
+
+export default Signin;
